3bd6bc09b000ae9e62f1461998ff21edaaa9fc74
Bubbad <Granen@hotmail.com>
2013-02-08 19:53:30 +0100

    Added comments

--- src/World/Tile.java
+++ src/World/Tile.java
@@ -1,14 +1,21 @@
 package World;
 
-public class Tile {
+import Utility.Entity;
+
+public class Tile extends Entity{
 	private int id;
 	
 	/**
 	 * 
 	 * @param id The tiles ID number
+	 * @param x The X coordinate of the tile
+	 * @param y The Y coordinate of the tile
+	 * @param width The width
+	 * @param height The height
 	 */
-	public Tile(int id)
+	public Tile(int id, int x, int y, int width, int height)
 	{
+		super(x, y, width, height);
 		this.id = id;		
 	}
 	
