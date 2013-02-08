7749de586cfef518c9d37f13728d89657f212b47
Bubbad <Granen@hotmail.com>
2013-02-08 19:47:18 +0100

    Added Tile class

--- src/World/Tile.java
+++ src/World/Tile.java
@@ -1,5 +1,20 @@
 package World;
 
 public class Tile {
-
+	private int id;
+	
+	/**
+	 * 
+	 * @param id The tiles ID number
+	 */
+	public Tile(int id)
+	{
+		this.id = id;		
+	}
+	
+	public int getId()
+	{
+		return id;		
+	}
+	
 }
