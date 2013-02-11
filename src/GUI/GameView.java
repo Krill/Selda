e74b1c095e90edb9bd16e752d889f33c88cf9fef
kristoffer <petersson.krill@gmail.com>
2013-02-11 18:33:57 +0100

    added runnable and empty constructor

--- src/GUI/GameView.java
+++ src/GUI/GameView.java
@@ -1,5 +1,25 @@
 package GUI;
 
-public class GameView {
+/**
+ * GameView
+ * @author kristoffer & johan
+ */
+public class GameView implements Runnable{
 
+	/**
+	 * Constructor
+	 */
+	public GameView(){
+		// Shit
+	}
+	
+	@Override
+	public void run() {
+		while(true){
+			
+			// Here goes the things that should be updated constantly...
+			
+			try {Thread.sleep(10);} catch (InterruptedException e) {e.printStackTrace();}
+		}	
+	}
 }
