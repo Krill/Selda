25afe64b2fa6278d557858160dd1fe7dc7ec7563
kristoffer <petersson.krill@gmail.com>
2013-02-11 19:09:07 +0100

    Added a window that does nothing

--- src/GUI/GameView.java
+++ src/GUI/GameView.java
@@ -1,21 +1,47 @@
 package GUI;
 
+import java.awt.Color;
+import javax.swing.JFrame;
+
 import Engine.GameEngine;
 
 /**
  * GameView
  * @author kristoffer & johan
  */
-public class GameView implements Runnable{
+@SuppressWarnings("serial")
+public class GameView extends JFrame implements Runnable{
 
 	// fields:
 	private GameEngine gameEngine;
+	
+	// constants:
+	private static final String GAME_TITLE = "GAMETITLE";
+	private static final int SCREEN_WIDTH = 806;
+	private static final int SCREEN_HEIGHT = 668;	
 	
 	/**
 	 * Constructor
 	 */
 	public GameView(GameEngine gameEngine){
 		this.gameEngine = gameEngine;
+		
+		makeFrame();
+	}
+	
+	/**
+	 * Creates the window
+	 */
+	private void makeFrame(){
+		pack();
+		
+		setTitle(GAME_TITLE);
+		setSize(SCREEN_WIDTH, SCREEN_HEIGHT);
+		setBackground(Color.BLACK);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setLocationRelativeTo(null);
+
+		setVisible(true);
 	}
 	
 	@Override
