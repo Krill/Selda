32f0a642404f676204fa857cbad3b399a4194a0c
Krill <petersson.krill@gmail.com>
2013-02-12 13:25:03 +0100

    updated

--- src/Controller/KeyboardController.java
+++ src/Controller/KeyboardController.java
@@ -39,24 +39,21 @@ public class KeyboardController extends KeyAdapter{
 	public void keyReleased(KeyEvent key){
 		
 		switch (key.getKeyCode()){
-			case KeyEvent.VK_UP:
-				player.setUp(false);
-				break;
-			case KeyEvent.VK_LEFT:
-				player.setLeft(false);
-				break;
-			case KeyEvent.VK_RIGHT:
-				player.setRight(false);
-				break;
-			case KeyEvent.VK_DOWN:
-				player.setDown(false);
-				break;
-				
-				
-				// Testing
-			case KeyEvent.VK_SPACE:
-				System.out.println(player.getX() + ", " + player.getY());
-				break;
+		case KeyEvent.VK_UP:
+			player.setUp(false);
+			break;
+		case KeyEvent.VK_LEFT:
+			player.setLeft(false);
+			break;
+		case KeyEvent.VK_RIGHT:
+			player.setRight(false);
+			break;
+		case KeyEvent.VK_DOWN:
+			player.setDown(false);
+			break;
+		case KeyEvent.VK_SPACE:
+			System.out.println(player.getX() + ", " + player.getY());
+			break;
 		}
 	}		
 }
