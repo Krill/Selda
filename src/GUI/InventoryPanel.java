724167fab858bc638f2627496330ec50aa97e4c5
kristoffer <petersson.krill@gmail.com>
2013-02-18 22:13:02 +0100

    fixes

--- src/GUI/InventoryPanel.java
+++ src/GUI/InventoryPanel.java
@@ -2,25 +2,21 @@ package GUI;
 
 import java.awt.Color;
 import java.awt.GridLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
-
 import javax.swing.BorderFactory;
-import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.border.LineBorder;
 
 import Character.PlayerCharacter;
-import Character.ShopCharacter;
 import Item.Item;
 
 /**
  * Show the players inventory
  * @author kristoffer
  */
+@SuppressWarnings("serial")
 public class InventoryPanel extends JPanel{
 
 	/**
