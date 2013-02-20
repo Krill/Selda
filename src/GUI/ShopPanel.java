d632b56babb4a97eb7afd337dc2f80fcf827dcf1
kristoffer <petersson.krill@gmail.com>
2013-02-20 19:00:48 +0100

    sad

--- src/GUI/ShopPanel.java
+++ src/GUI/ShopPanel.java
@@ -1,12 +1,18 @@
 package GUI;
 
 import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
 import java.awt.GridLayout;
+import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 import javax.swing.BorderFactory;
+import javax.swing.Box;
+import javax.swing.BoxLayout;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
@@ -27,12 +33,15 @@ public class ShopPanel extends JPanel{
 	
 	// fields:
 	
+	// constants:
+	private static final String PANEL_BACKGROUND = "images/gui/shop.png";	
+	
 	/**
 	 * Constructor
 	 */
 	public ShopPanel(){
 		setPanelDetails();
-
+		
 		// Add a KeyListener to this window when active
 		addKeyListener(new KeyAdapter(){
 			@Override
@@ -46,11 +55,20 @@ public class ShopPanel extends JPanel{
 	 * Sets this panels default visuals
 	 */
 	private void setPanelDetails(){
-		setBorder(BorderFactory.createCompoundBorder(new LineBorder(Color.BLACK), BorderFactory.createEmptyBorder(5, 5, 5, 5)));
+		setOpaque(true);
 		setDoubleBuffered(true);
-		setBounds(200, 220, 400, 200);
+		setBounds(100, 300, 600, 300);
 		setVisible(false);
+		setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
 		setFocusable(true);
+	}
+	
+	/**
+	 * Paints a background image
+	 */
+	public void paintComponent(Graphics g) {
+		Image img = new ImageIcon(PANEL_BACKGROUND).getImage();	
+		g.drawImage(img, 0, 0, null);
 	}
 
 	/**
@@ -59,7 +77,7 @@ public class ShopPanel extends JPanel{
 	 * @param arg
 	 */
 	public void update(ShopCharacter shop, PlayerCharacter player) {
-		
+
 		// cancel players movement
 		player.setUp(false);
 		player.setDown(false);
@@ -68,90 +86,27 @@ public class ShopPanel extends JPanel{
 		
 		// remove old shops and set new content
 		removeAll();
-		setContent(shop, player);
+		updateShop(shop, player);
 		
 		// make visible and set focus
-		setLocation(shop.getX(),shop.getY());
 		setVisible(true);
 		requestFocusInWindow();	
 	}
 	
 	/**
-	 * Sets the new content
+	 * Updates the shop with items
 	 * @param shop
 	 * @param player
 	 */
-	private void setContent(ShopCharacter shop, final PlayerCharacter player){
-		setLayout(new GridLayout(shop.getInventory().size() + 1, 1));
+	private void updateShop(ShopCharacter shop, PlayerCharacter player){
 		
-		// Set titles
-		JPanel titleRow = new JPanel();
-		titleRow.setLayout(new GridLayout(1, 3));
-		JLabel titleName = new JLabel("Name:");
-		JLabel titleValye = new JLabel("Value:");
-		JLabel titleBuy = new JLabel("Buy:");
-		titleRow.add(titleName);
-		titleRow.add(titleValye);
-		titleRow.add(titleBuy);
-		add(titleRow);
+		// fill out shop label
+		add(Box.createVerticalStrut(50));
 		
-		for(final Item item : shop.getInventory()){
-			JPanel row = new JPanel();
-			row.setLayout(new GridLayout(1, 3));
-			
-			final JLabel itemName = new JLabel(item.getName());
-			row.add(itemName);
-			
-			JLabel itemValue = new JLabel(""+item.getItemValue());
-			row.add(itemValue);			
-			
-			JButton b = new JButton("Buy!");
-			b.addActionListener(new ActionListener(){
-				public void actionPerformed(ActionEvent e){
-					buyItem(item, player);
-					requestFocusInWindow();
-				}
-			});
-			
-			row.add(b);
-			add(row);
-		}
-	}
-	
-	/**
-	 * Buys an Item from the shop
-	 * @param item
-	 */
-	private void buyItem(Item item, PlayerCharacter player){
-		
-		int response = JOptionPane.showConfirmDialog(this, "Are you sure you want to buy:\n" + item.getName() + "\nfor " + item.getItemValue() + " money", "Confirm!", JOptionPane.YES_NO_OPTION);
-		
-		if(response == JOptionPane.YES_OPTION){
-			// If player has enough money
-			if(player.getMoney() >= item.getItemValue()){
-				
-				// If player has space left in his inventory
-				if(player.getCurrentInventorySize() < player.getMaxInventorySize()){
-					System.out.println("Du har nu köpt: " + item.getName());
-					
-					Item newItem = item.clone();
-					player.addToInventory(newItem);
-									
-					// ugly solutuiob
-					int invID = player.getCurrentInventorySize() - 1;
-					Item inItem = player.getInventory().get(invID);					
-					inItem.setId(invID);
-					
-		
-					player.setMoney(player.getMoney() - newItem.getItemValue());
-				} else {
-					System.out.println("Your inventory is full!");
-					JOptionPane.showMessageDialog(this, "Your inventory is full!");
-				}
-			} else {
-				System.out.println("You cannot afford this item!");
-				JOptionPane.showMessageDialog(this, "You cannot afford this item!");
-			}
-		}
+		JPanel shopPanel = new JPanel();
+		shopPanel.setPreferredSize(new Dimension(600, 200));
+//		shopPanel.setBorder(new LineBorder(Color.BLACK));
+		shopPanel.setOpaque(false);
+		add(shopPanel);
 	}
 }
